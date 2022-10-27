3963daf4a4a8cd0d5d887a7d2c37aff8ddefae0b
史振宁 <160416011@qq.com>
2022-10-27 11:49:49 +0800

    AAA

--- mytools/filetools.py
+++ mytools/filetools.py
@@ -8,7 +8,7 @@ class fileTool:
     # python删除文件的方法 os.remove(path)path指的是文件的绝对路径,如：
     def del_file(self, path_data):
         for i in os.listdir(path_data):  # os.listdir(path_data)#返回一个列表，里面是当前目录下面的所有东西的相对路径
-            file_data = path_data + "\\" + i  # 当前文件夹的下面的所有东西的绝对路径
+            file_data = path_data + "/" + i  # 当前文件夹的下面的所有东西的绝对路径
             if os.path.isfile(file_data) == True:  # os.path.isfile判断是否为文件,如果是文件,就删除.如果是文件夹.递归给del_file.
                 os.remove(file_data)
             else:
